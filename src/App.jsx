c7b682f60065f1acfed6e276febb10e0e4222408
Misael Taperia <askedmisael2@gmail.com>
2025-08-28 02:05:39 -0500

    added transparent header, blurred, with logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Header from './components/header'
+import pfp from './assets/images/josh pfp.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      hello world
-    </>
+    <div>
+      <Header />
+      <img src={pfp} alt="" />
+      <img src={pfp} alt="" />
+      <img src={pfp} alt="" />
+    </div>
   )
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import pfp from "../assets/images/josh pfp.png";
+const Header = () => {
+  return (
+      <div className="Header">
+        
+        <div className="MainLogo">
+        <img src={pfp} alt="josh" />
+      </div>
+
+      <div className="Logo">
+       <h2 className="ftj">Felix Josh</h2>
+       <h2 className="pht">P h o t o g r a p h y</h2>
+      </div>
+      
+      </div>
+      
+    
+  );
+};
+
+export default Header;
